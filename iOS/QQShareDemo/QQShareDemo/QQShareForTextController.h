b5d4830b5c35ea0a745d16f79dbd944b2619612d
zhenyong <zyenong@126.com>
2016-10-12 23:18:13 +0800

    u[pdate

--- iOS/QQShareDemo/QQShareDemo/QQShareForTextController.h
+++ iOS/QQShareDemo/QQShareDemo/QQShareForTextController.h
@@ -10,4 +10,5 @@
 
 @interface QQShareForTextController : UIViewController
 
+
 @end
--- iOS/QQShareDemo/QQShareDemo/QQShareForTextController.m
+++ iOS/QQShareDemo/QQShareDemo/QQShareForTextController.m
@@ -7,15 +7,19 @@
 //
 
 #import "QQShareForTextController.h"
+#import <QuartzCore/QuartzCore.h>
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/TencentApiInterface.h>
+#import <TencentOpenAPI/QQApiInterfaceObject.h>
+#import <TencentOpenAPI/QQApiInterface.h>
 
-@interface QQShareForTextController ()
+
+@interface QQShareForTextController ()<UITextViewDelegate,UITextFieldDelegate>
+
 @property (weak, nonatomic) IBOutlet UITextField *objTitle;
-@property (weak, nonatomic) IBOutlet UITextView *objDesc;
-@property (weak, nonatomic) IBOutlet UITextView *objText;
 
-@property (weak, nonatomic) IBOutlet UITextView *objUrl;
 - (IBAction)onBtnConfirmPressed:(UIButton *)sender;
-- (IBAction)onBtnCancelPressed:(UIButton *)sender;
+- (IBAction)onBtnShareToQQZone:(UIButton *)sender;
 
 
 
@@ -25,14 +29,90 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [[TencentOAuth alloc] initWithAppId:@"1104663467" andDelegate:self];
+    self.objTitle.delegate = self;
+   }
+
+- (IBAction)onBtnConfirmPressed:(UIButton *)sender {
+    QQApiTextObject *txtObj = [QQApiTextObject objectWithText:self.objTitle.text];
+    SendMessageToQQReq *req = [SendMessageToQQReq reqWithContent:txtObj];
+    QQApiSendResultCode sent = [QQApiInterface sendReq:req];
+    [self handleSendResult:sent];
+
+}
+
+- (IBAction)onBtnShareToQQZone:(UIButton *)sender {
+    QQApiImageArrayForQZoneObject *obj = [QQApiImageArrayForQZoneObject objectWithimageDataArray:nil title:self.objTitle.text];
+    SendMessageToQQReq *req = [SendMessageToQQReq reqWithContent:obj];
+    QQApiSendResultCode sent = [QQApiInterface SendReqToQZone:req];
+    [self handleSendResult:sent];
     
 }
 
 
-
-- (IBAction)onBtnConfirmPressed:(UIButton *)sender {
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return NO;
 }
 
-- (IBAction)onBtnCancelPressed:(UIButton *)sender {
+- (void)handleSendResult:(QQApiSendResultCode)sendResult
+{
+    switch (sendResult)
+    {
+        case EQQAPIAPPNOTREGISTED:
+        {
+            UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"App未注册" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+            [msgbox show];
+            
+            
+            break;
+        }
+        case EQQAPIMESSAGECONTENTINVALID:
+        case EQQAPIMESSAGECONTENTNULL:
+        case EQQAPIMESSAGETYPEINVALID:
+        {
+            UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"发送参数错误" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+            [msgbox show];
+            
+            
+            break;
+        }
+        case EQQAPIQQNOTINSTALLED:
+        {
+            UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"未安装手Q" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+            [msgbox show];
+            
+            
+            break;
+        }
+        case EQQAPIQQNOTSUPPORTAPI:
+        {
+            UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"API接口不支持" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+            [msgbox show];
+            
+            
+            break;
+        }
+        case EQQAPISENDFAILD:
+        {
+            UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"发送失败" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+            [msgbox show];
+            
+            
+            break;
+        }
+        case EQQAPIVERSIONNEEDUPDATE:
+        {
+            UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"当前QQ版本太低，需要更新" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+            [msgbox show];
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
 }
+
 @end
